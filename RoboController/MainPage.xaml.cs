6291b914358345d48cf3a8d46535fb10ee46f6bc
dirk.koehler <dirk.koehler@cardioscan.de>
2018-06-07 14:52:34 +0200

    implementation of first gpio control

--- RoboController/MainPage.xaml.cs
+++ RoboController/MainPage.xaml.cs
@@ -7,13 +7,24 @@ using Windows.ApplicationModel.Core;
 using Windows.UI.Core;
 using Windows.UI.Xaml.Controls;
 using NetworkLibrary;
+using Windows.Devices.Gpio;
+using Windows.UI.Xaml;
 
 namespace RoboController
 {
     public sealed partial class MainPage : Page
     {
 
-        DeviceClient deviceClient;
+        private int _pinEn1_2 = 21;
+        private int _pin1A = 20;
+        private int _pin2A = 16;
+
+        private GpioController _controller;
+        private GpioPin _motorEnable;
+        private GpioPin _motorControl1A;
+        private GpioPin _motorControl2A;
+
+        private DeviceClient _deviceClient;
 
         public static async Task CallOnUiThreadAsync(CoreDispatcher dispatcher, DispatchedHandler handler) =>
         await dispatcher.RunAsync(CoreDispatcherPriority.Normal, handler);
@@ -24,8 +35,17 @@ namespace RoboController
         public MainPage()
         {
             this.InitializeComponent();
-            deviceClient = DeviceClient.CreateFromConnectionString(GlobalConstant.DEVICE_CONNECTION_STRING, TransportType.Mqtt);
-            deviceClient.SetMethodHandlerAsync("Drive", Drive, null);
+
+            _controller = GpioController.GetDefault();
+            _motorEnable = _controller.OpenPin(_pinEn1_2);
+            _motorControl1A = _controller.OpenPin(_pin1A);
+            _motorControl2A = _controller.OpenPin(_pin2A);
+            _motorEnable.SetDriveMode(GpioPinDriveMode.Output);
+            _motorControl1A.SetDriveMode(GpioPinDriveMode.Output);
+            _motorControl2A.SetDriveMode(GpioPinDriveMode.Output);
+
+            _deviceClient = DeviceClient.CreateFromConnectionString(GlobalConstant.DEVICE_CONNECTION_STRING, TransportType.Mqtt);
+            _deviceClient.SetMethodHandlerAsync("Drive", Drive, null);
         }
 
         private async Task<MethodResponse> Drive(MethodRequest methodRequest, object userContext)
@@ -35,9 +55,84 @@ namespace RoboController
             await CallOnMainViewUiThreadAsync(() =>
             {
                 IncomingMessage.Text = "drive"+" "+"direction: "+ cmdData.direction + " "+"speed: " + cmdData.speed + " " + "distance: " + cmdData.distance;
+                TurnOnIgnition();
+                if (cmdData.direction == 0)
+                {
+                    ReverseMotor();
+                } else if(cmdData.direction == 1)
+                {
+                    ForwardMotor();
+                }
             });
             string result = "{\"result\":\"Executed direct method: " + methodRequest.Name + "\"}";
             return new MethodResponse(Encoding.UTF8.GetBytes(result), 200);
+        }
+
+        private void TurnOnIgnition()
+        {
+            btnIgnitionOn.IsEnabled = false;
+            btnIgnitionOff.IsEnabled = true;
+            btnForward.IsEnabled = true;
+            btnReverse.IsEnabled = true;
+            _motorEnable.Write(GpioPinValue.High);
+        }
+
+        private void ForwardMotor()
+        {
+            btnForward.IsEnabled = false;
+            btnReverse.IsEnabled = true;
+            _motorControl1A.Write(GpioPinValue.High);
+            _motorControl2A.Write(GpioPinValue.Low);
+        }
+
+        private void ReverseMotor()
+        {
+            btnReverse.IsEnabled = false;
+            btnForward.IsEnabled = true;
+            _motorControl1A.Write(GpioPinValue.Low);
+            _motorControl2A.Write(GpioPinValue.High);
+        }
+
+        private void StopMotor()
+        {
+            _motorControl1A.Write(GpioPinValue.Low);
+            _motorControl2A.Write(GpioPinValue.Low);
+        }
+
+        private void TurnOffIgnition()
+        {
+            btnIgnitionOn.IsEnabled = true;
+            btnIgnitionOff.IsEnabled = false;
+            btnForward.IsEnabled = false;
+            btnReverse.IsEnabled = false;
+            _motorEnable.Write(GpioPinValue.Low);
+            _motorControl1A.Write(GpioPinValue.Low);
+            _motorControl2A.Write(GpioPinValue.Low);
+        }
+
+        private void BtnIgnitionOn_Click(object sender, RoutedEventArgs e)
+        {
+            TurnOnIgnition();
+        }
+
+        private void BtnIgnitionOff_Click(object sender, RoutedEventArgs e)
+        {
+            TurnOffIgnition();
+        }
+
+        private void BtnForward_Click(object sender, RoutedEventArgs e)
+        {
+            ForwardMotor();
+        }
+
+        private void BtnReverse_Click(object sender, RoutedEventArgs e)
+        {
+            ReverseMotor();
+        }
+
+        private void BtnStop_Click(object sender, RoutedEventArgs e)
+        {
+            StopMotor();
         }
 
     }
